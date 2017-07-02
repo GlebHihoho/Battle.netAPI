d2dc8f260fbdedb6db3169dcb17695e277f80cd3
Gleb Motuz <hihoho@mail.ru>
2017-07-02 12:14:39 +0200

    SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,57 @@
 import React      from 'react';
 import ReactDOM   from 'react-dom';
+import axios      from 'axios';
 
 import CardList   from './components/CardList';
+import SearchBar  from './components/SearchBar';
 
-function App() {
-  return (
-    <div>
-      <CardList />
-    </div>
-  )
+const cardUrl = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards';
+const headers = { "X-Mashape-Key" : "ZDXXrbn7fhmshIHGnLYdPiKozzLGp1rFAPujsns0ULQjtcrmpJ" };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Basic : ''
+    }
+
+    this.update = {
+      Basic : ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value.toLowerCase();
+
+    const newBasic = Object.assign({}, this.state).Basic.filter(card => card.name.toLowerCase().includes(value));
+
+    this.setState({
+      Basic : newBasic
+    })
+    // this.update.Basic = newBasic;
+
+    console.log(this.state.Basic)
+  }
+
+  componentDidMount() {
+    axios.get(cardUrl, { headers })
+          .then(res => res.data)
+          .then(res => this.setState({
+            Basic : res.Basic
+          }))
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onStatusChange={this.handleChange} />
+        <CardList Basic={this.state.Basic} />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,42 +1,18 @@
-import React, { Component } from 'react';
-import axios    from 'axios';
+import React from 'react';
+import Card  from './Card';
 
-import Card from './Card';
+function CardList(props) {
+  const { Basic } = props;
 
-const cardUrl = 'https://omgvamp-hearthstone-v1.p.mashape.com/cards';
-const headers = { "X-Mashape-Key" : "ZDXXrbn7fhmshIHGnLYdPiKozzLGp1rFAPujsns0ULQjtcrmpJ" }
-
-class CardList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      Basic : ''
-    }
-  }
-
-  componentDidMount() {
-    axios.get(cardUrl, { headers })
-          .then(res => res.data)
-          .then(res => this.setState({
-            Basic : res.Basic
-          }))
-  }
-
-  render() {
-    console.log(this.state.Basic);
-
-    return (
-      <div className="card-list">
-
-        {
-          !this.state.Basic
-            ? <div className="loader">Loading...</div>
-            : this.state.Basic.map(card => <Card key={card.cardId} card={card}/>)
-        }
-      </div>
-    )
-  }
+  return (
+    <div className="card-list">
+      {
+        !Basic
+          ? <div className="loader">Loading...</div>
+          : Basic.map(card => <Card key={card.cardId} card={card}/>)
+      }
+    </div>
+  )
 }
 
 export default CardList;
